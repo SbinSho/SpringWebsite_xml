cfa6af6a9f4b56e81eee99b63993cbbc9076acf7
SbinSho <suho0232@gmail.com>
2021-02-17 22:24:19 +0900

    start spring_project

---
+++ src/main/java/com/suho/web/homeController.java
@@ -0,0 +1,14 @@
+package com.suho.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController{
+
+	@RequestMapping("/home")
+	public String home() {
+		return "/WEB-INF/views/home.jsp";
+	}
+	
+}
